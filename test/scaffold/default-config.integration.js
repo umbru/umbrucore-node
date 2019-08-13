b106f4f81c8cf2f5a8e6f1200b43ca810c43791b
Ryxor <team@umbru.io>
2019-08-13 21:20:33 +1200

    Update Ports

--- test/scaffold/default-config.integration.js
+++ test/scaffold/default-config.integration.js
@@ -20,7 +20,7 @@ describe('#defaultConfig', function() {
         umbrud: {
           connect: [{
             rpchost: '127.0.0.1',
-            rpcport: 9998,
+            rpcport: 12354,
             rpcuser: 'umbru',
             rpcpassword: 'local321',
             zmqpubrawtx: 'tcp://127.0.0.1:28332'
@@ -66,7 +66,7 @@ describe('#defaultConfig', function() {
         umbrud: {
           connect: [{
             rpchost: '127.0.0.1',
-            rpcport: 9998,
+            rpcport: 12354,
             rpcuser: 'umbru',
             rpcpassword: 'local321',
             zmqpubrawtx: 'tcp://127.0.0.1:28332'
